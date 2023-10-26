a7a34a43c13ce1324efa0118dc861b2241a20dae
RonieMuller1976 <145708692+RonieMuller1976@users.noreply.github.com>
2023-10-26 15:08:19 -0300

    main

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const listaDeTeclas = document.querySelectorAll(".tecla");
 //criar a variavel de contagem
 let contador = 0;
 //laço de repetição While
-while(contador < 9){
+while(contador < listaDeTeclas.length){
 //chamada para tocar o som pelo índice do elemento da lista
 listaDeTeclas[contador].onclick = tocaSomAplausos;
     contador = contador + 1;
