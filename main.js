4e1476fca0b0496cee5161ae5a1c12d78efada65
RonieMuller1976 <145708692+RonieMuller1976@users.noreply.github.com>
2023-10-26 15:24:56 -0300

    main

--- main.js
+++ main.js
@@ -9,8 +9,11 @@ const listaDeTeclas = document.querySelectorAll(".tecla");
 let contador = 0;
 //laço de repetição While
 while(contador < listaDeTeclas.length){
-//chamada para tocar o som pelo índice do elemento da lista
-listaDeTeclas[contador].onclick = tocaSomAplausos;
+const efeito = listaDeTeclas [contador].classList[1];
+const idAudio = "#som_" + efeito;
+listaDeTeclas[contador].onclick = function(){
+    tocaSom(idAudio);
+}
     contador = contador + 1;
     console.log(contador);
 }
