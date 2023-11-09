c1e3146326f69034202c61ca5f2c0f6792e3e138
RonieMuller1976 <145708692+RonieMuller1976@users.noreply.github.com>
2023-11-09 15:05:24 -0300

    template_som

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ const listaDeTeclas = document.querySelectorAll(".tecla");
 
 //criar a variavel de contagem
 let contador = 0;
-//laço de repetição While
-while(contador < listaDeTeclas.length){
+//laço de repetição While trocado pelo "for" tecla, efeito, idAudio e TocaSom 
+for(let contador = 0; contador < listaDeTeclas.length; contador = contador +1){
+  const tecla = listaDeTeclas;
 const efeito = listaDeTeclas [contador].classList[1];
-const idAudio = "#som_" + efeito;
-listaDeTeclas[contador].onclick = function(){
+const idAudio = `#som_${efeito}`;//uso do template string
+tecla.onclick = function(){
     tocaSom(idAudio);
 }
-    contador = contador + 1;
-    console.log(contador);
+
 }
